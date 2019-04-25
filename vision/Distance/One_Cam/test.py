16a77f2afb5475aba80c6eff5cb7e97cd8969c28
Robin Hogerhuis <robin@mbp-van-robin.access.nhl.nl>
2019-04-25 12:03:10 +0200

    multiple colors detection

--- vision/Distance/One_Cam/test.py
+++ vision/Distance/One_Cam/test.py
@@ -1,19 +1,41 @@
-
 import cv2
 import numpy as np
 
 cam = cv2.VideoCapture(0)
-lower_blue = np.array([110,50,50])
-upper_blue = np.array([130,255,255])
+boundaries = [
+              ([ 15, 183, 116], [ 35, 203, 196]),
+              ([ 26, 123, 164], [ 36, 133, 204])
+              ]
+
+# loop over the boundaries
+lower_yellow = np.array([16, 133, 121])
+upper_yellow = np.array([36, 153, 201])
+
 
 while (True):
-    ret, frame = cam.read()
-
+    _, frame = cam.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
-    res = cv2.bitwise_and(frame,frame, mask= mask)
+    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    
+    for (lower, upper) in boundaries:
+        lower = np.array(lower, dtype = "uint8")
+        upper = np.array(upper, dtype = "uint8")
 
-    cv2.imshow('camera', res)
+        #find the colors within the specified boundaries and apply the mask
+        in_mask = cv2.inRange(hsv, lower, upper)
+        mask = mask + in_mask
+
+    output = cv2.bitwise_and(frame, frame, mask = mask)
+    cv2.imshow('images', output)
+
+    #mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    #res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    #kernel = np.ones((15, 15), np.float32)/225
+    #smooted = cv2.filter2D(res, -1, kernel)
+
+    #cv2.imshow('camera', res)
+    #cv2.imshow('smooted', smooted)
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
