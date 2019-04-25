3cdcbe282f1f206d8257b178a390b046d5802edc
Robin Hogerhuis <robin@mbp-van-robin.access.nhl.nl>
2019-04-25 14:49:10 +0200

    Yellow and blue recognition

--- vision/Distance/One_Cam/test.py
+++ vision/Distance/One_Cam/test.py
@@ -1,16 +1,17 @@
 import cv2
 import numpy as np
+import os, sys
 
 cam = cv2.VideoCapture(0)
+
+#Add color blue to mask
 boundaries = [
-              ([ 15, 183, 116], [ 35, 203, 196]),
-              ([ 26, 123, 164], [ 36, 133, 204])
-              ]
+            ([110,50,50], [130,255,255])
+         ]
 
 # loop over the boundaries
-lower_yellow = np.array([16, 133, 121])
-upper_yellow = np.array([36, 153, 201])
-
+lower_yellow = np.array([13, 123, 116])
+upper_yellow = np.array([36, 265, 236])
 
 while (True):
     _, frame = cam.read()
@@ -28,14 +29,6 @@ while (True):
     output = cv2.bitwise_and(frame, frame, mask = mask)
     cv2.imshow('images', output)
 
-    #mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-    #res = cv2.bitwise_and(frame,frame, mask= mask)
-
-    #kernel = np.ones((15, 15), np.float32)/225
-    #smooted = cv2.filter2D(res, -1, kernel)
-
-    #cv2.imshow('camera', res)
-    #cv2.imshow('smooted', smooted)
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
