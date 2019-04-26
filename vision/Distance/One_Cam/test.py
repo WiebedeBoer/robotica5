39b3d088809d9bc9bb37637f00647337bba2076c
Robin Hogerhuis <robin@mbp-van-robin.access.nhl.nl>
2019-04-26 15:45:47 +0200

    testing

--- vision/Distance/One_Cam/test.py
+++ vision/Distance/One_Cam/test.py
@@ -5,18 +5,27 @@ import math
 
 cam = cv2.VideoCapture(0)
 
+# Distance calculating
+    # Initialize the known distance from the camera to the object
+KNOWN_DISTANCE = 100.0
+    # Initialize the known object width, which in this case
+KNOWN_WIDTH = 15.0
+  # Initialize the known object focal length, which in this case
+FOCAL_LENGTH = 1013
+
+# Text settings
 font                   = cv2.FONT_HERSHEY_SIMPLEX
 bottomLeftCornerOfText = (10,500)
 fontScale              = 0.75
 fontColor              = (255,255,255)
 lineType               = 2
 
-#Add color blue to mask
+# Add color blue to mask
 boundaries = [
-            ([100,150,0], [140,255,255])
+              #([100,150,0], [140,255,255])
          ]
 
-# loop over the boundaries
+# Loop over the boundaries
 lower_yellow = np.array([20, 100, 100])
 upper_yellow = np.array([30, 255, 255])
 
@@ -30,10 +39,10 @@ def main():
             lower = np.array(lower, dtype = "uint8")
             upper = np.array(upper, dtype = "uint8")
 
-            #find the colors within the specified boundaries and apply the mask
+            # Find the colors within the specified boundaries and apply the mask
             in_mask = cv2.inRange(hsv, lower, upper)
             mask = mask + in_mask
-
+    
         output = cv2.bitwise_and(frame, frame, mask = mask)
         contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -42,26 +51,42 @@ def main():
             perimeter = cv2.arcLength(cnt, True)
             x, y, w, h = cv2.boundingRect(cnt)
             if(area > 1600):
+                # Set contour arround object
                 frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 255), 2)
                 
-                cv2.putText(frame,str(str(w) + " " + str(h)),
+                # Distance to object calculating
+                distance = calculateDistance(w)
+                
+                # (312 - 160) / 50 = 3.04
+                # 464 - (3.04 * distance)
+                # 0.048 / 50 = (3 / 3125)
+                relLength = (3 / 3125) * distance # Calculating relatieve length
+                KNOWN_WIDTH = (((w + distance * 3.04) - (3.04 * distance)) * relLength) # First = calculation at 0 cm // Second = calc what width is at that distance
+                
+                # Calibration
+                #focalLength = calibration(w)
+                
+                # Apply text in frame
+                cv2.putText(frame,str(str(w)+ " " + str(KNOWN_WIDTH) + " " + str(h) + " " + str(distance)),
                             (x + w, y + h),
                             font,
                             fontScale,
                             fontColor,
                             lineType)
-
-
+                
+        #Show frame
         cv2.imshow('images', frame)
-
+        #Check if "esc" is pressed
         k = cv2.waitKey(1) & 0xFF
         if k == 27:
             break
-
+    
     cv2.destroyAllWindows()
 
-def distance_to_camera(knownWidth, focalLength, perWidth):
-    # compute and return the distance from the maker to the camera
-    return (knownWidth * focalLength) / perWidth
+def calibration(w):
+    return (KNOWN_DISTANCE * (w / 2)) / (KNOWN_WIDTH / 2)
+
+def calculateDistance(w):
+    return (FOCAL_LENGTH * (KNOWN_WIDTH / 2)) / (w / 2)
 
 main()
