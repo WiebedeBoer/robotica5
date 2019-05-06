b75177f49eccdcd4481da4a2030499bcb29f3a5a
Kris van den Berg <krisberg@home.nl>
2019-05-06 11:15:40 +0200

    xbee code arduino

---
+++ xbeeconnection.ino
@@ -0,0 +1,19 @@
+void setup()  {
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Goodnight moon!");
+  }
+ 
+ 
+ 
+void loop()                     // run over and over again
+{
+  bool res = false;
+  while (Serial.available()) {
+      Serial.print((char)Serial.read());
+      res = true;
+  }
+  if (res)
+  Serial.print("\n");
+  delay(100);
+}
