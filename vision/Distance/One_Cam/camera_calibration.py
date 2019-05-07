0bea3f0089ce876c21987c26e03ad363bae04b6b
Robin Hogerhuis <robin@mbp-van-robin.access.nhl.nl>
2019-05-07 11:17:03 +0200

    added picutes schaakbord

--- vision/Distance/One_Cam/camera_calibration.py
+++ vision/Distance/One_Cam/camera_calibration.py
@@ -2,36 +2,72 @@ import numpy as np
 import cv2
 import glob
 
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# Define the chess board rows and columns
+rows = 6
+cols = 8
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+# Set the termination criteria for the corner sub-pixel algorithm
+criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 30, 0.001)
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+# Prepare the object points: (0,0,0), (1,0,0), (2,0,0), ..., (6,5,0). They are the same for all images
+objectPoints = np.zeros((rows * cols, 3), np.float32)
+objectPoints[:, :2] = np.mgrid[0:rows, 0:cols].T.reshape(-1, 2)
 
-images = glob.glob('*.jpg')
+# Create the arrays to store the object points and the image points
+objectPointsArray = []
+imgPointsArray = []
 
-for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# Loop over the image files
+for path in glob.glob('pictures/camera_calibration/schaakbord[0-1][0-5].jpg'):
+    # Load the image and convert it to gray scale
+    img = cv2.imread(path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+    ret, corners = cv2.findChessboardCorners(gray, (rows, cols), None)
     
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
+    # Make sure the chess board pattern was found in the image
+    if ret:
+        # Refine the corner position
+        corners = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         
-        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        imgpoints.append(corners2)
+        # Add the object points and the image points to the arrays
+        objectPointsArray.append(objectPoints)
+        imgPointsArray.append(corners)
         
-        # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-        cv2.imshow('img',img)
-        cv2.waitKey(500)
+        # Draw the corners on the image
+        cv2.drawChessboardCorners(img, (rows, cols), corners, ret)
+    
+    # Display the image
+    cv2.imshow('chess board', img)
+    cv2.waitKey(500)
 
+# Calibrate the camera and save the results
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objectPointsArray, imgPointsArray, gray.shape[::-1], None, None)
+np.savez('../data/calib.npz', mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
+
+# Print the camera calibration error
+error = 0
+
+for i in range(len(objectPointsArray)):
+    imgPoints, _ = cv2.projectPoints(objectPointsArray[i], rvecs[i], tvecs[i], mtx, dist)
+    error += cv2.norm(imgPointsArray[i], imgPoints, cv2.NORM_L2) / len(imgPoints)
+
+print("Total error: ", error / len(objectPointsArray))
+
+# Load one of the test images
+img = cv2.imread('pictures/camera_calibration/schaakbord1.jpeg')
+h, w = img.shape[:2]
+
+# Obtain the new camera matrix and undistort the image
+newCameraMtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+undistortedImg = cv2.undistort(img, mtx, dist, None, newCameraMtx)
+
+# Crop the undistorted image
+# x, y, w, h = roi
+# undistortedImg = undistortedImg[y:y + h, x:x + w]
+
+# Display the final result
+cv2.imshow('chess board', np.hstack((img, undistortedImg)))
+cv2.waitKey(0)
 cv2.destroyAllWindows()
