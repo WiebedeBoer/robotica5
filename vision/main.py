c6791d3726ff67c95cab4cde8d3ff3fb697ef170
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-05-08 14:41:03 +0200

    main 1e opzet
    main

---
+++ vision/main.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+import sys
+import time
+
+#EI OPPAKKEN EN BRENGEN NAAR BAKJE MET QR
+
+#ei detection
+from egg import ShowCamFeed
+ShowCamFeed()
+
+#QR detection
+from qrdetector import QRdetect
+QRdetect()
+
+#bakjes detection
+from test_all_colors import Bakje
+Bakje()
+
+#kip detection
+from chicken_run import Kip
+Kip()
+
+#POORT KWALIFICATIE
+from poort_run import Poort
+Poort()
+
+#PATH FINDING
+
+#pathfinder algoritme
+from pathfinding import FindPath
+FindPath()
+
+#HELLING RACE
+
+#trap detection
+from trap import TrapOp
+TrapOp()
