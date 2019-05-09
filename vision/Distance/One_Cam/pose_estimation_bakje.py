ca5de6d831e2cceb273ab55e124b79b99230d3f6
Robin Hogerhuis <robin@mbp-van-robin.access.nhl.nl>
2019-05-09 09:54:16 +0200

    delete maps and added pose bakje

--- vision/Distance/One_Cam/pose_estimation_bakje.py
+++ vision/Distance/One_Cam/pose_estimation_bakje.py
@@ -1,12 +1,115 @@
-import numpy as np
+
 import cv2
-import glob
-import os
-import argparse
+import numpy as np
+from decimal import *
 
 cam = cv2.VideoCapture(0)
 cam.set(3,640)
 cam.set(4,480)
+
+# Define the chess board rows and columns
+rows = 8
+cols = 6
+
+# Load the camera calibration data
+with np.load('pictures/camera_calibration/calib.npz') as calibData:
+    mtx, dist, rvecs, tvecs = [calibData[i] for i in ('mtx', 'dist', 'rvecs', 'tvecs')]
+
+# Initialize the known distance from the camera to the object
+KNOWN_DISTANCE = 100.0
+# Initialize the known object width, which in this case
+KNOWN_WIDTH = Decimal(15.0)
+# Initialize the known object focal length, which in this case
+FOCAL_LENGTH = 1013
+
+# Text settings
+font = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText = (10, 500)
+fontScale = 0.75
+fontColor = (255, 255, 255)
+lineType = 2
+
+# Add color blue to mask
+boundaries = [
+    [([20, 100, 100], [30, 255, 255])],  # Yellow
+    #[([100, 150, 0], [140, 255, 255])],  # Blue
+    [([20, 100, 100], [30, 255, 255])]  # Red
+]
+
+totalContours = []
+
+# Set the termination criteria for the corner sub-pixel algorithm
+criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 30, 0.001)
+
+
+# Prepare the object points: (0,0,0), (1,0,0), (2,0,0), ..., (6,5,0). They are the same for all images
+objectPoints = np.zeros((rows * cols, 1, 3), np.float32)
+objectPoints[:, :, :2] = np.mgrid[0:rows, 0:cols].T.reshape(-1, 1, 2)
+
+# Create the axis points
+axisPoints = np.float32([[0,0,0], [0,3,0], [3,3,0], [3,0,0],
+                   [0,0,-3],[0,3,-3],[3,3,-3],[3,0,-3] ])
+
+
+def main():
+    while (1):
+        _, frame = cam.read()
+        img = cv2.GaussianBlur(frame, (15, 15), 0)
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        for range in boundaries:
+            in_mask = None;
+            for (lower, upper) in range:
+                # Find the colors within the specified boundaries and apply the mask
+                if (in_mask is None):
+                    in_mask = cv2.inRange(hsv, np.array(lower, dtype="uint8"), np.array(upper, dtype="uint8"));
+                else:
+                    in_mask = in_mask + cv2.inRange(hsv, np.array(lower, dtype="uint8"), np.array(upper, dtype="uint8"))
+                _, res = cv2.threshold(in_mask, 215, 255, cv2.THRESH_BINARY)
+                res = cv2.GaussianBlur(res, (15, 15), 0)
+                contours, hierarchy = cv2.findContours(res, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                totalContours.append(contours)
+
+        for contours in totalContours:
+            for cnt in contours:
+                #area = cv2.contourArea(cnt)
+                perimeter = cv2.arcLength(cnt, True)
+                approx = cv2.approxPolyDP(cnt, 0.04 * perimeter, True)
+                x, y, w, h = cv2.boundingRect(cnt)
+
+                if (w > 40 and h > 10 and len(approx) >= 4 and len(approx) <= 6):
+                    # Set contour arround object
+                    frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 255), 2)
+                    print("In latest if statement")
+                   #frame = pose_estimation(frame)
+
+                    # Distance to object calculating
+                    distance = calculateDistance(w)
+
+                    # Apply text in frame
+                    cv2.putText(frame, str(str(w) + " " + str(h) + " " + str(round(distance, 2))),
+                                (x + (w / 2), y), font, fontScale, fontColor, lineType)
+
+        del totalContours[:]
+
+        # Show frame
+        cv2.imshow('camera', frame)
+
+        # Check if "esc" is pressed
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+
+    cv2.destroyAllWindows()
+
+
+def calibration(w):
+    return (KNOWN_DISTANCE * (w / 2)) / (KNOWN_WIDTH / 2)
+
+
+def calculateDistance(w):
+    return (FOCAL_LENGTH * (KNOWN_WIDTH / 2)) / (w / 2)
+
+
 # This function draws lines joining the given image points to the first chess board corner
 def draw(frame, corners, imgpts):
     imgpts = np.int32(imgpts).reshape(-1,2)
@@ -19,28 +122,9 @@ def draw(frame, corners, imgpts):
     frame = cv2.drawContours(frame, [imgpts[4:]],-1,(0,0,255),3)
     return frame
 
-# Load the camera calibration data
-with np.load('pictures/camera_calibration/calib.npz') as calibData:
-    mtx, dist, rvecs, tvecs = [calibData[i] for i in ('mtx', 'dist', 'rvecs', 'tvecs')]
 
-# Define the chess board rows and columns
-rows = 8
-cols = 6
-
-# Set the termination criteria for the corner sub-pixel algorithm
-criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 30, 0.001)
-
-# Prepare the object points: (0,0,0), (1,0,0), (2,0,0), ..., (6,5,0). They are the same for all images
-objectPoints = np.zeros((rows * cols, 1, 3), np.float32)
-objectPoints[:, :, :2] = np.mgrid[0:rows, 0:cols].T.reshape(-1, 1, 2)
-
-# Create the axis points
-axisPoints = np.float32([[0,0,0], [0,3,0], [3,3,0], [3,0,0],
-                   [0,0,-3],[0,3,-3],[3,3,-3],[3,0,-3] ])
-
-while(1):
+def pose_estimation(frame):
     # Load the image and convert it to gray scale
-    _, frame = cam.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Find the chess board corners
@@ -59,11 +143,6 @@ while(1):
 
         # Draw the axis lines
         frame = draw(frame, corners, axisImgPoints)
+        return frame
 
-    # Display the image
-
-    cv2.imshow('chess board', frame)
-    if (cv2.waitKey(1) & 0xFF) == 27:
-        break
-
-cv2.destroyAllWindows()
+main()
