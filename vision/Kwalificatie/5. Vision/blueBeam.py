e059a0145140e13cbba4ec7b219d9613311bc2d2
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-05-14 15:10:17 +0200

    rgb
    rgb

--- vision/Kwalificatie/5. Vision/blueBeam.py
+++ vision/Kwalificatie/5. Vision/blueBeam.py
@@ -9,12 +9,16 @@ def ViewBeam():
         #25,87,154 = 211,84,60 = 211,214,153 #27,94,164 = 211,84,64 = 211,214,163
         #([100,150,0], [140,255,255])
         # go over the boundaries, blue beam
-        lower_blue = np.array([211,214,153])
-        upper_blue = np.array([211,214,163])
+        #([0,140,44],[100,238,240])
+        #lower_blue = np.array([211,214,153])
+        #upper_blue = np.array([211,214,163])
+        lower_blue = np.array([0,140,44])
+        upper_blue = np.array([100,238,240])
         #mask
         hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
         mask = cv2.inRange(hsv, lower_blue, upper_blue)
         #contours
+        output = cv2.bitwise_and(frame, frame, mask = mask)
         contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         for cnr in range(len(contours)):
             cnt = contours[cnr]
@@ -25,7 +29,7 @@ def ViewBeam():
             else:
                 factor = 0
             x, y, w, h = cv2.boundingRect(cnt)
-            if(area >200 and area <3600 and factor >0 and factor <0.88):
+            if(area >100 and factor >0 and factor <0.99):
                 frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 2)
 
         #show vid
