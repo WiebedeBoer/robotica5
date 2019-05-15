f95336d1e835ae183593010d0d6822584e82dbd0
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-05-15 12:13:08 +0200

    w + h in print
    w + h in print

--- vision/Kwalificatie/5. Vision/blueBeam.py
+++ vision/Kwalificatie/5. Vision/blueBeam.py
@@ -1,8 +1,18 @@
 import cv2
 import numpy as np
+import os, sys
 import math
+from decimal import *
+import glob
 
 def ViewBeam():
+    # Text settings
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    bottomLeftCornerOfText = (10,500)
+    fontScale = 0.75
+    fontColor = (255,255,255)
+    lineType = 2
+    #video capture
     cap = cv2.VideoCapture(0)
     while(1):
         _, frame = cap.read()
@@ -30,23 +40,53 @@ def ViewBeam():
                 factor = 0
             x, y, w, h = cv2.boundingRect(cnt)
             if(area >100 and area <3600 and factor >0 and factor <0.99):
+                #set contour around object
                 frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 2)
+                #calculate distance
+                distance = calculateDistance(w)
                 #print command
-                print("FollowBeam;")
+                print("FollowBeam;"+str(w)+";"+str(h)+"\n")
             elif(area >3600 and factor >0 and factor <0.99):
+                #set contour around object
                 frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                #calculate distance
+                distance = calculateDistance(w)
+                #apply text
+                cv2.putText(frame,str(str(w)+ " " + str(h) + " " + str(round(distance, 2))),(x + (w / 2), y),font,fontScale,fontColor,lineType)
                 #print command
-                print("StopBeam;")
-            else:
+                print("StopBeam;\n")
+            #else:
                 #print command
-                print("SearchBeam;")
-
+                print("SearchBeam;\n")
         #show vid
         cv2.imshow("Frame", frame)
-
+        #key
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cap.release()
     cv2.destroyAllWindows()
 
+#calibrate distance
+def calibration(w):
+    # Initialize the known distance from the camera to the object
+    KNOWN_DISTANCE = 100.0
+    # Initialize the known object width, which in this case
+    KNOWN_WIDTH = Decimal(15.0)
+    # Initialize the known object focal length, which in this case
+    FOCAL_LENGTH = 1013
+    #return calculation
+    return (KNOWN_DISTANCE * (w / 2)) / (KNOWN_WIDTH / 2)
+
+#calculate distance
+def calculateDistance(w):
+    # Initialize the known distance from the camera to the object
+    KNOWN_DISTANCE = 100.0
+    # Initialize the known object width, which in this case
+    KNOWN_WIDTH = Decimal(15.0)
+    # Initialize the known object focal length, which in this case
+    FOCAL_LENGTH = 1013
+    #return calculation
+    return (FOCAL_LENGTH * (KNOWN_WIDTH / 2)) / (w / 2)
+
+#beam function
 ViewBeam()
