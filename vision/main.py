cafaebf793d5e90bbcd66c24134b7b943b12d084
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-05-15 12:57:10 +0200

    socket eerst nog even commented
    socket eerst nog even commented

--- vision/main.py
+++ vision/main.py
@@ -7,7 +7,24 @@ import time
 from client import SocketReceive
 SocketReceive()
 Qualify = SocketReceive()
-#Qualify ="EggRace"
+
+#socket
+#import socket
+#sok = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#sok.connect((socket.gethostname(), 1234))
+
+#def SocketReceive():
+#    msg_send = "Ask"
+#    utf8_msg = unicode(msg_send, "utf-8")
+    # output
+#    sok.send(bytes(utf8_msg))
+#    rec = sok.recv(1024, 0)
+#    return rec
+
+#def SocketSend(msg):
+#    utf8_msg = unicode(msg, "utf-8")
+
+#sok.send(bytes(utf8_msg))
 
 #SWITCH CASE QUALIFY OR RACE
 def switchQualify(Qualify):
