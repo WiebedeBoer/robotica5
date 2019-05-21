5f91f4e4dee9aadeb53e780b0502b33b49193e83
Robin Hogerhuis <robin@mbp-van-robin.access.nhl.nl>
2019-05-21 11:47:19 +0200

    changed

--- vision/camera_opencv.py
+++ vision/camera_opencv.py
@@ -3,11 +3,11 @@ from picamera import PiCamera
 
 # Camera init
 camera = PiCamera()
-camera.resolution = (640, 480)
+camera.resolution = (1280, 720)
 camera.framerate = 30
 
 def getPiCamera():
-    rawCapture = PiRGBArray(camera, size=(640, 480))
+    rawCapture = PiRGBArray(camera, size=(1280, 720))
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=1):
         rawCapture.truncate()
         rawCapture.seek(0)
