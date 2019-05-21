5dc5dcf8631570019b0bd135f495dcf132afc43e
Robin Hogerhuis <robin@mbp-van-robin.access.nhl.nl>
2019-05-21 11:11:00 +0200

    changed camera_opencv

--- vision/camera_opencv.py
+++ vision/camera_opencv.py
@@ -1,20 +1,12 @@
-# import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
+import picamera
+import picamera.array
 
 
 def getPiCamera():
-    # initialize the camera and grab a reference to the raw camera capture
-    camera = PiCamera()
-    return camera
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1280, 720)
+        with picamera.array.PiRGBArray(camera, size=(640, 360)) as output:
+            frame = camera.capture(output, 'rgb', resize=(640, 360))
+            return frame
 
-    # camera.resolution = (640, 480)
-    # camera.framerate = 32
-    # rawCapture = PiRGBArray(camera, size=(640, 480))
-    #
-    # # allow the camera to warmup
-    # time.sleep(0.1)
-    # # capture frames from the camera
-    # for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    #     return frame.array
+        
