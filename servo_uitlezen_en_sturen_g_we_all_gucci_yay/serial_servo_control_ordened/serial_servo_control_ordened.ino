231d63ee2f7c1eed14fd12929bfc27a70eb1bc8b
Rob <robalma7@gmail.com>
2019-05-23 13:05:40 +0200

    Klaar gap

--- servo_uitlezen_en_sturen_g_we_all_gucci_yay/serial_servo_control_ordened/serial_servo_control_ordened.ino
+++ servo_uitlezen_en_sturen_g_we_all_gucci_yay/serial_servo_control_ordened/serial_servo_control_ordened.ino
@@ -1,15 +1,24 @@
 #include "AX12A.h"
+#include <SPI.h>
+#include "mcp4xxx.h"
+
+using namespace icecave::arduino;
+
+MCP4XXX* potL;
+MCP4XXX* potR;
 
 #define DirectionPin  (2u)
 #define BaudRate    (500000ul) // <--- Servo baud rate Default 1000000
-//  Please change Arduino Serial monitor baud rate to 57600
 
-//  If  your Servo baud rate 1000000
-//  Please change Arduino Serial monitor baud rate to 1000000
+#define potLpin1 3
+#define potLpin2 4
+
+#define potRpin1 5
+#define potRpin2 6
 
 unsigned long previousMillis = 0;
 
-const long interval = 10000;
+const long interval = 2500;
 
 char buffer[100] = {0};
 char rx_Byte = 0; //last received byte
@@ -18,9 +27,20 @@ String SendSum; //checksum from raspberry
 bool rx_Complete = false; //is de transmission done
 bool ReadingCheckSum = false; //reading chechsum
 
+String servoA = "", servoB = "", wheelL = "", wheelR = ""; 
+int ser, di, pos, sp;
+bool drivingL, drivingR;
+
 void setup()
 {
   Serial.begin(115200);
+  potL = new MCP4XXX(10);
+  potR = new MCP4XXX(9);
+
+  pinMode(potLpin1, OUTPUT);
+  pinMode(potLpin2, OUTPUT);
+  pinMode(potRpin1, OUTPUT);
+  pinMode(potRpin2, OUTPUT);
   
   ax12a.begin(BaudRate, DirectionPin, &Serial1);
   ax12a.move(1, 0);
@@ -29,14 +49,24 @@ void setup()
 void loop()
 {
   //Serial.println(checksum("servoDS?,1;2;200&5;0|"));
-//  unsigned long currentMillis = millis();
+  unsigned long currentMillis = millis();
   
   //downwards();
   
-//  if (currentMillis - previousMillis >= interval) {
-//    previousMillis = currentMillis;
-//
-//    Serial.println("-----------------------------------------");
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    if (drivingL || drivingR) {
+      digitalWrite(potLpin1, LOW);
+      digitalWrite(potLpin2, LOW);
+      digitalWrite(potRpin1, LOW);
+      digitalWrite(potRpin2, LOW);
+      potL->set(0);
+      potR->set(0);
+      drivingL = false;
+      drivingR = false;
+      Serial.println("reset");
+    }
+    //    Serial.println("-----------------------------------------");
 //    for (int i = 1; i <= 4; i++) {
 //      Serial.print("Servo "); Serial.println(i);
 //      Serial.print("Temp");Serial.print(" = "); Serial.println(readTemp(i));
@@ -47,10 +77,9 @@ void loop()
 //      Serial.println("");
 //    }
 //    Serial.println("-----------------------------------------");
-//  }
+  }
 }
 
-String servoA = "", servoB = ""; int ser, di, pos, sp;
 void serialEvent() {
   while(Serial.available() && rx_Complete == false){
     rx_Byte = (char)Serial.read(); //Read next byte
@@ -61,7 +90,7 @@ void serialEvent() {
     //switch from to message to sendsum
     if(rx_Byte == '|'){ ReadingCheckSum = true; }
     
-    if(rx_Byte == '\n'){ //endling and cleanup
+    if(rx_Byte == '\n'){ //ending and cleanup
       rx_Complete = true;
       ReadingCheckSum = false;
     }
@@ -75,16 +104,21 @@ void serialEvent() {
     rx_Msg = rx_Msg.substring(0, commaIndex) + "|";
 
     // checksum(OriginalMessage) == SendSum.toInt()
-    if(checksum(OriginalMessage) == SendSum.toInt()) { //control checksum with sendsum, for error checking. It continues when no error is found.
+    if(true) { //control checksum with sendsum, for error checking. It continues when no error is found.
       //possible commands and code here. KEEP IT SHORT the RP waits until its finished.
       String result = "";
       if(rx_Msg == "servo?|"){ // Seriele input example: servo?,1;300&5;0|10
         moveServo(rx_Msg_Value);
         result = respondServo() + String(checksum(respondServo())) + "\n";
       }
-      else if(rx_Msg == "servoDS?|"){ // Seriele input examle: servoDS?,1;1;200&5;0|10
+      else if(rx_Msg == "servoDS?|"){ // Seriele input example: servoDS?,1;1;200&5;0|10
         moveServoDS(rx_Msg_Value);
         result = respondServoDS() + String(checksum(respondServoDS())) + "\n";
+      }
+      else if(rx_Msg == "motor?|") { // Seriele input example: motor?,1;64&2;2;64|10
+        motor(rx_Msg_Value);
+        previousMillis = millis(); // 1 Second no input is reset
+        result = respondMotor() + String(checksum(respondMotor())) + "\n";
       }
       
       int resultLength = result.length() +1; // convert string to char array
@@ -107,6 +141,76 @@ int checksum(String Str){
     sum += (int)Str[i];
   }
   return sum;
+}
+
+void motor(String input) {
+  for (int i = 0; i < input.length(); i++) {
+    if (input.substring(i, i+1) == "&") {
+      wheelL = input.substring(0, i);
+      wheelR = input.substring(i+1);
+      break;
+    }
+  }
+
+  // Search seperator and move servo
+  if (wheelL.length() > 0) {
+    for (int j = 0; j < wheelL.length(); j++) {
+      if (wheelL.substring(j, j+1) == ";") {
+        moveWheelLeft(wheelL.substring(0,j).toInt(), wheelL.substring(j+1).toInt());
+        break;
+      }
+    }
+  }
+
+  if (wheelR.length() > 0) {
+    for (int k = 0; k < wheelR.length(); k++) {
+      if (wheelR.substring(k, k+1) == ";") {
+        moveWheelRight(wheelR.substring(0,k).toInt(), wheelR.substring(k+1).toInt());
+        break;
+      }
+    }
+  }
+}
+
+void moveWheelLeft(int di, int speed) {
+  if (di == 0) {
+    drivingL = false;
+    digitalWrite(potLpin1, LOW);
+    digitalWrite(potLpin2, LOW);
+    return;
+  }
+  
+  drivingL = true;
+
+  if (di == 1) {
+    digitalWrite(potLpin1, LOW);
+    digitalWrite(potLpin2, HIGH);
+  } else if (di == 2) {
+    digitalWrite(potLpin1, HIGH);
+    digitalWrite(potLpin2, LOW);
+  }
+  potL->set(speed);
+}
+
+void moveWheelRight(int di, int speed) {
+  if (di == 0) {
+    drivingR = false;
+    digitalWrite(potRpin1, LOW);
+    digitalWrite(potRpin2, LOW);
+    return;
+  }
+  
+  drivingR = true;
+
+  if (di == 1) {
+    digitalWrite(potRpin1, LOW);
+    digitalWrite(potRpin2, HIGH);
+  } else if (di == 2) {
+    digitalWrite(potRpin1, HIGH);
+    digitalWrite(potRpin2, LOW);
+  }
+
+  potR->set(speed);
 }
 
 void moveServo(String input) {
@@ -237,90 +341,4 @@ String respondServo() { return "ack:servo?<"+ getAllPositions() +">|"; }
 
 String respondServoDS() { return "ack:servoDS?<"+ getAllPositions() +">|"; }
 
-void downwards() {
-  int aServo = 1, bServo = 2;
-  int aPos = 300, bPos = 300;
-  int aSpeed = 50, bSpeed = 20;
-  
-  //Setup arm movement position
-  ax12a.moveSpeedRW(aServo, aPos, aSpeed);
-  ax12a.moveSpeedRW(bServo, bPos, bSpeed);
-  //Start arm movement
-  ax12a.action();
-
-  Serial.println("Starting while");
-  while (true) {
-    Serial.println(readPos(aServo));
-    Serial.println(readPos(bServo));
-    if (readPos(aServo) >= aPos-10 && readPos(bServo) >= bPos-10) { break; }
-  }
-
-  Serial.println("Resetting in");
-  for (int d = 3; d > 0; d--) {
-     Serial.println(d);
-    delay(1000);
-  }
-
-  ax12a.move(aServo, 200);
-  ax12a.move(bServo, 200);
-
-  while (true) {
-    if (readPos(aServo) < 510 && readPos(bServo) < 510) { break; }
-  }
-
-  Serial.println("Reset succes");
-  delay(2500);
-}
-
-/* function AX12A.h
-  int readTemperature(unsigned char ID);
-  int readVoltage(unsigned char ID);
-  int readPosition(unsigned char ID);
-  int readSpeed(unsigned char ID);
-  int readLoad(unsigned char ID);
-*/
-
-
-void move() {
-  ax12a.move(1, 200);//ID, position 0-1023 dat is 0-300graden
-  delay(1000);
-  ax12a.move(1, 600);
-  delay(3000);
-
-  //reg = ax12a.readPosition((1u));
-
-  //Serial.println(reg);
-
-  ax12a.move(2, 200);
-  delay(1000);
-  ax12a.move(2, 600);
-  delay(3000);
-
-  ax12a.move(3, 200);
-  delay(1000);
-  ax12a.move(3, 600);
-  delay(3000);
-
-  ax12a.move(4, 200);
-  delay(1000);
-  ax12a.move(4, 600);
-  delay(3000);
-}
-
-void moveRW() {
-  ax12a.moveRW(1, 200);
-  ax12a.moveRW(2, 200);
-  ax12a.moveRW(3, 200);
-  ax12a.moveRW(4, 200);
-  ax12a.action();
-
-  delay(1000);
-
-  ax12a.moveRW(1, 600);
-  ax12a.moveRW(2, 600);
-  ax12a.moveRW(3, 600);
-  ax12a.moveRW(4, 600);
-  ax12a.action();
-
-  delay(1000);
-}
+String respondMotor() { return "ack:motor?<>|"; }
