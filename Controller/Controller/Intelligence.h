75935249c575f6ac78d87991c6cd751a69f022e4
smit1712 <32259732+smit1712@users.noreply.github.com>
2019-05-29 14:07:12 +0200

    stoi trycatch added

--- Controller/Controller/Intelligence.h
+++ Controller/Controller/Intelligence.h
@@ -20,5 +20,25 @@ private:
 	MicroController* Worker;
 	MicroController* Sensor;
 	Vision* VisionApi;
+
+	int RefreshInterfal = 1000;
+	int PrintInterfal = 100000000;
+	int ArmInterfal = 10000000;
+	int DriveInterfal = 2000000;
+	int VisionInterfall = 10000;
+
+	std::pair<int, int>* joy1 = new std::pair<int, int>(Database->GetJoy1());
+	std::pair<int, int>* joy2 = new std::pair<int, int>(Database->GetJoy2());
+
+	std::chrono::system_clock::time_point refreshAfstandBediening = std::chrono::system_clock::now() + std::chrono::milliseconds(RefreshInterfal);
+	std::chrono::system_clock::time_point PrintJoystick = std::chrono::system_clock::now() + std::chrono::milliseconds(PrintInterfal);
+	std::chrono::system_clock::time_point MoveArm = std::chrono::system_clock::now() + std::chrono::milliseconds(ArmInterfal);
+	std::chrono::system_clock::time_point Drive = std::chrono::system_clock::now() + std::chrono::milliseconds(DriveInterfal);
+	std::chrono::system_clock::time_point RefreshVision = std::chrono::system_clock::now() + std::chrono::milliseconds(VisionInterfall);
+
+	void CheckDrive();
+	void CheckVision();
+	void CheckAfstandbediening();
+	void CheckArm();
 };
 
