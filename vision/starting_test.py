8d825ce783d9998bacc30bbd8757253e2838dbe7
Robske <robalma7@gmail.com>
2019-06-06 13:35:12 +0100

    New function starting.py

--- vision/starting_test.py
+++ vision/starting_test.py
@@ -1,20 +1,16 @@
 import sys
 import socket
 import cv2
-from camera_opencv import getPiCamera
+#from camera_opencv import getPiCamera
 import numpy
-import os
-#Onwindows use:
-#os.chdir(os.path.realpath(__file__+ '\\..\\'))
-#OnLinux Use:
-os.chdir(os.path.realpath(__file__+ '//..//'))
+
 # Here the function gets the string send by the pi
 def SocketReceive():
     rec = sok.recv(1024, 0)
     return rec
 
 
-# Here the function sends a string ba ck to the pi
+# Here the function sends a string back to the pi
 def SocketSend(msg):
     utf8_msg = unicode(str(msg), "utf-8")
     sok.send(bytes(utf8_msg))
@@ -73,7 +69,7 @@ def eggtelligence(argument, argument1, frame):
         return startEggDistance(frame)
 
     def qrDistance(argument, frame):
-        sys.path.append('Wedstrijd/Eggtelligence/')        
+        sys.path.append('Wedstrijd/Eggtelligence/')
         from startQRDistance import startQRDistance
         return startQRDistance(townSwitcher(argument), frame)
 
@@ -90,35 +86,40 @@ def eggtelligence(argument, argument1, frame):
         return func
 
     return eggtelligenceSwitcher(argument, argument1, frame)
-# cap = cv2.VideoCapture(0)
-# try:
-#     while True:
-#         # frame = getCapture()
-#         _, frame = cap.read()
-#         print(mainSwitcher(3, 1, 0, frame))
-#         # cv2.imshow("frame", frame)
-#         if cv2.waitKey(1) & 0xFF == ord('q'):
-#             break
-#
-# except KeyboardInterrupt:
-#     cap.release()
-#     cv2.destroyAllWindows()
 
-sok = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+cap = cv2.VideoCapture(0)
 try:
-    sok.connect((socket.gethostname(), 1234))
-
     while True:
-    	frame = getPiCamera()
-        msg = SocketReceive()
-        msg = splitter(msg)
+         # frame = getCapture()
+        _, frame = cap.read()
+        print(mainSwitcher(4, 0, 0, frame))
+        # cv2.imshow("frame", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-        msgBack = mainSwitcher(int(msg[0]), int(msg[1]), int(msg[2]), frame)
-        SocketSend(msgBack)
+except KeyboardInterrupt:
+    cap.release()
+    cv2.destroyAllWindows()
 
-except Exception, e:
-    sok.close()
-    print e
+#sok = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#try:
+#    sok.connect((socket.gethostname(), 1234))
 
-finally:
-    sok.close()
+#   while True:
+#    	frame = getPiCamera()
+#       msg = SocketReceive()
+#        msg = splitter(msg)
+
+#        msgBack = mainSwitcher(int(msg[0]), int(msg[1]), int(msg[2]), frame)
+#        msgBack = 0:4:0:0
+ #       SocketSend(msgBack)
+
+#except Exception, e:
+#    sok.close()
+#    print e
+
+#finally:
+#sok.close()
+
