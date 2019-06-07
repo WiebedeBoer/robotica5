6fec93843de94b4857cdb3ab4f7bb29ff1b793f1
Wiebe de Boer <wiebe81@gmail.com>
2019-06-07 14:23:10 +0200

    out vector for distance and coordinate

--- Controller/Controller/Intelligence.cpp
+++ Controller/Controller/Intelligence.cpp
@@ -110,16 +110,16 @@ void Intelligence::CheckEgg() {
 	}
 }
 
-/*
+
 //splitting distance from coordinate
-template <class OutIt>
-void explode(std::string const &input, char sep, OutIt output){
+
+void explode(std::string const &input, char sep, std::vector<std::string>& output) {
 	std::istringstream buffer(input);
 	std::string temp;
 	while (std::getline(buffer, temp, sep))
-		*output++ = temp;
+		output.push_back(temp);
 }
-*/
+
 
 //blue beam vision qualification
 void Intelligence::CheckBlueBeam() {
@@ -128,11 +128,11 @@ void Intelligence::CheckBlueBeam() {
 	int bluedistance = 0; //distance
 	int horizontal = 0; //horizontal coordinate
 	if (!Intelligence::Database->kwalificatie.vision.empty()) {
-		//std::string s = Intelligence::Database->kwalificatie.vision;
-		//std::vector<std::string> out;
-		//explode(s, ':', out);
+		std::string s = Intelligence::Database->kwalificatie.vision;
+		std::vector<std::string> out;
+		explode(ref(s), ':', ref(out));
 
-
+		/*
 			try {
 				bluedistance = std::stoi(Intelligence::Database->kwalificatie.eiGripper);
 			}
@@ -140,26 +140,23 @@ void Intelligence::CheckBlueBeam() {
 				std::cout << "stoi error occurred. Exception" << e << '\n';
 				bluedistance = 999;
 			}
-
-			/*
+			*/
+			
 		try {			
 			bluedistance = std::stoi(out[0]);			
 		}
 		catch (int e) {
 			std::cout << "stoi distance error occurred. Exception" << e << '\n';
 			bluedistance = 999;			
-		}
-		*/
-
-		/*
+		}	
+		
 		try {
 			horizontal = std::stoi(out[1]);
 		}
 		catch (int e) {
 			std::cout << "stoi coordinate error occurred. Exception" << e << '\n';
 			horizontal = 0;
-		}
-		*/
+		}		
 
 		if (bluedistance != 0 && bluedistance != NULL) {
 			//if near and in sight, drive
