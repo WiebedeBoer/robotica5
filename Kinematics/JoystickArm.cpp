198a6613de99cf6165e95116613fedce58dbb1c5
Wiebe de Boer <wiebe81@gmail.com>
2019-06-11 14:32:10 +0200

    angle to pin comment

--- Kinematics/JoystickArm.cpp
+++ Kinematics/JoystickArm.cpp
@@ -6,22 +6,24 @@
 #include "JoystickArm.h"
 
 Node JoystickArm::GetPathsVertical() {
-    std::vector<Node &> possibilities;
-    for (int angle0 = VerticalServos[0].Angle - 30; angle0 < VerticalServos[0].Angle + 30; angle0++) {
-        if (angle0 >= VerticalServos[0].MinRotation and angle0 <= VerticalServos[0].MaxRotation) {
-            Node *node0 = new Node(angle0, VerticalServos[0]);
-            for (int angle1 = VerticalServos[1].Angle - 30; angle1 < VerticalServos[1].Angle + 30; angle1++) {
-                if (angle1 >= VerticalServos[1].MinRotation and angle1 <= VerticalServos[1].MaxRotation) {
-                    Node *node1 = new Node(angle1, VerticalServos[1], node0);
-                    for (int angle2 = VerticalServos[2].Angle - 30; angle2 < VerticalServos[2].Angle + 30; angle2++) {
-                        if (angle2 >= VerticalServos[2].MinRotation and angle2 <= VerticalServos[2].MaxRotation) {
-                            Node *node2 = new Node(angle2, VerticalServos[2], node1);
-                            possibilities.push_back(node2)
-                        }
-                    }
-                }
-            }
-        }
-    }
-    delete *possibilities;
+	std::vector<Node &> possibilities;
+	for (int angle0 = VerticalServos[0].Angle - 30; angle0 < VerticalServos[0].Angle + 30; angle0++) {
+		if (angle0 >= VerticalServos[0].MinRotation and angle0 <= VerticalServos[0].MaxRotation) {
+			Node *node0 = new Node(angle0, VerticalServos[0]);
+			for (int angle1 = VerticalServos[1].Angle - 30; angle1 < VerticalServos[1].Angle + 30; angle1++) {
+				if (angle1 >= VerticalServos[1].MinRotation and angle1 <= VerticalServos[1].MaxRotation) {
+					Node *node1 = new Node(angle1, VerticalServos[1], node0);
+					for (int angle2 = VerticalServos[2].Angle - 30; angle2 < VerticalServos[2].Angle + 30; angle2++) {
+						if (angle2 >= VerticalServos[2].MinRotation and angle2 <= VerticalServos[2].MaxRotation) {
+							Node *node2 = new Node(angle2, VerticalServos[2], node1);
+							possibilities.push_back(node2)
+						}
+					}
+					//AngleToPin(angle, servo); //angle to pin for a particular servo
+				}
+			}
+		}
+	}
+	delete *possibilities;
 }
+
