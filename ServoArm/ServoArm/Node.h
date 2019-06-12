cf2093b0d91dfdb44da51354d4741586098c6399
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-06-12 12:06:31 +0200

    kinematics project
    kinematics project

---
+++ ServoArm/ServoArm/Node.h
@@ -0,0 +1,29 @@
+#pragma once
+//
+// Created by Wouter Nauta on 2019-06-07.
+//
+
+#ifndef KINEMATICS_NODE_H
+#define KINEMATICS_NODE_H
+
+#include <vector>
+#include "math.h"
+#include "Servo.h"
+
+class Node {
+public:
+	Node(int _angle, const Servo& _servo, Node* _last);
+	Node(int _angle, const Servo& _servo);
+	~Node();
+	double GetLength();
+	double GetHeight();
+	int AngleChanges();
+	int Angle;
+private:
+	Node* Last;
+	const Servo& ServoArm;
+
+};
+
+
+#endif //KINEMATICS_NODE_H
