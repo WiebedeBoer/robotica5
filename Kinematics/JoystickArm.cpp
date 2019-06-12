59b2d261575f9497e62db79bc56714436ee50d28
Wouter Nauta <naut1701@student.nhl.nl>
2019-06-12 12:14:01 +0200

    Fix

--- Kinematics/JoystickArm.cpp
+++ Kinematics/JoystickArm.cpp
@@ -6,21 +6,21 @@
 #include "JoystickArm.h"
 
 Node JoystickArm::GetPathsVertical(int length, int height) {
-    std::vector<Node &> possibilitiesExact;
-    std::vector<Node &> possibilitiesOther;
+    std::vector<Node *> possibilitiesExact;
+    std::vector<Node *> possibilitiesOther;
     for (int angle0 = VerticalServos[0].MinRotation; angle0 < VerticalServos[0].MaxRotation; angle0++) {
         Node *node0 = new Node(angle0, VerticalServos[0]);
         for (int angle1 = VerticalServos[1].MinRotation; angle1 < VerticalServos[1].MaxRotation + 30; angle1++) {
             Node *node1 = new Node(angle1, VerticalServos[1], node0);
             for (int angle2 = VerticalServos[2].MinRotation; angle2 < VerticalServos[2].MaxRotation + 30; angle2++) {
-                Node *node2 = new Node(angle2, VerticalServos[2], node1);
+                Node* node2 = new Node(angle2, VerticalServos[2], node1);
                 double node2Length = node2->GetLength();
                 double node2Height = node2->GetHeight();
                 if(round(node2Length) == length and round(node2Height) == height){
                     if(node2Length == length or node2Height == height){
-                        possibilitiesExact.push_back(*node2);
+                        possibilitiesExact.push_back(node2);
                     }else{
-                        possibilitiesOther.push_back(*node2);
+                        possibilitiesOther.push_back(node2);
                     }
                 }else{
                     delete node2;
