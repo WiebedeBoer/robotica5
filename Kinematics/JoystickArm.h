679181d75b6e6d3a70ffd6d1efb2af152256a043
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-06-13 14:51:22 +0200

    horizontal servos
    horizontal servos

--- Kinematics/JoystickArm.h
+++ Kinematics/JoystickArm.h
@@ -1,6 +1,6 @@
 //
 // Created by Wouter Nauta on 2019-06-06.
-//
+// altered by Wiebe and Jesse
 
 #ifndef KINEMATICS_JOYSTICKARM_H
 #define KINEMATICS_JOYSTICKARM_H
@@ -13,22 +13,23 @@
 #include <algorithm>
 class JoystickArm {
 public:
-	Node* GetPathsVertical(int length, int height);
+    Node* GetPathsVertical(int length, int height);
 	int NodeToPin(Node node);
 private:
-	const int MIN_HEIGHT = 10;
-	const int MAX_HEIGHT = 50;
-	const int MAX_LENGTH = 100;
+    const int MIN_HEIGHT = 10;
+    const int MAX_HEIGHT = 50;
+    const int MAX_LENGTH = 100;
 
-	const std::vector<Servo> VerticalServos = std::vector<Servo>{
-			Servo(2, 10, 50, 23, 12),
-			Servo(3, 10, 50, 23, 12),
-			Servo(4, 10, 50, 23, 12)
-	};
+    const std::vector<Servo> VerticalServos = std::vector<Servo>{
+            Servo(2, 10, 50, 23, 12),
+            Servo(3, 10, 50, 23, 12),
+            Servo(4, 10, 50, 23, 12)						
+    };
 	const std::vector<Servo> HorizontalServo = std::vector<Servo>{
-			Servo(1, 10, 50, 23, 12),
-			Servo(5, 10, 50, 23, 12)
+		Servo(1, 10, 50, 23, 12),
+		Servo(5, 10, 50, 23, 12)		
 	};
 };
+
 
 #endif //KINEMATICS_JOYSTICKARM_H
