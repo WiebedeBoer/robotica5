1501b61c515629f5aee42916bff88b9cfe2f46ba
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-06-13 14:49:18 +0200

    length segments set for angle calculation
    length segments set for angle calculation

--- Kinematics/Angles.h
+++ Kinematics/Angles.h
@@ -1,6 +1,6 @@
 //
 // Created by Wouter Nauta on 2019-06-11.
-//
+// altered by Wiebe de Boer 2019-06-12
 
 #ifndef KINEMATICS_ANGLES_H
 #define KINEMATICS_ANGLES_H
@@ -15,8 +15,8 @@ public:
 private:
     double A1; //root joint
     double A2; //counter clockwise joint
-	double Len1; //arm segment 1
-	double Len2; //arm segment 2
+	double Len1 = 150.0; //arm segment 1
+	double Len2 = 196.0; //arm segment 2
     double D1; //dividing angle A1 joint
     double D2; //dividng angle A1 joint
     double Dist; //distance between tip of segment 2 and root joint
