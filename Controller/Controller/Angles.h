a59e070f66e6e10c6ca39f642c4650a04f219a5c
Wiebe de Boer <wiebe81@gmail.com>
2019-06-17 10:24:22 +0200

    met vector

--- Controller/Controller/Angles.h
+++ Controller/Controller/Angles.h
@@ -1,23 +1,25 @@
-#pragma once
 //
 // Created by Wouter Nauta on 2019-06-11.
-// altered by Wiebe de Boer 2019-06-12
+// altered by Wiebe de Boer
 
 #ifndef KINEMATICS_ANGLES_H
 #define KINEMATICS_ANGLES_H
 
 #include <math.h>
+#include <vector>
+
 class Angles {
 public:
-	double Gonio(double TipX, double TipY);
+	std::vector<int> Gonio(double TipX, double TipY);
 	double LawOfCosines(double& _a, double& _b, double& _c);
 	double Distance(double& _x, double& _y);
 	int AngleToPin(double Angle, int Servo);
 private:
+	std::vector<int> args;
 	double A1; //root joint
 	double A2; //counter clockwise joint
-	double Len1 = 150.0; //arm segment 1
-	double Len2 = 196.0; //arm segment 2
+	double Len1; //arm segment 1
+	double Len2; //arm segment 2
 	double D1; //dividing angle A1 joint
 	double D2; //dividng angle A1 joint
 	double Dist; //distance between tip of segment 2 and root joint
