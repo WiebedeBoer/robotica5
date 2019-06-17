e43fa12a47002fc746c43c015ee26d9313fc7d75
Wiebe de Boer <wiebe81@gmail.com>
2019-06-17 10:11:07 +0200

    angles

--- Controller/Controller/Command.cpp
+++ Controller/Controller/Command.cpp
@@ -1,5 +1,7 @@
 #include "Command.h"
 #include "Angles.h"
+#include <string>
+#include <stdio.h>
 
 Command::Command(Vision * v, std::string t, DataCollector* DC)
 {
@@ -72,8 +74,14 @@ void Command::Execute() {
 		return;
 	}
 	if (Command::type == "KineArmForward") {
-		Command::slave->SerialSend("servoS?,1;100;50&5;0;100|10");
-		std::cout << "The Arm is moving Backward!!!:" << args[0] << "," << args[1] << std::endl;
+		double Targetx = 5.0; //hardcoded target, must be from python vision
+		double Targety = 5.0;
+		Angles dlg;
+		char buffer[100];
+		std::vector<int> noutput = dlg.Gonio(Targetx, Targety);
+		sprintf(buffer, "servoS?,1;%d;50&5;0;%d", noutput[0], noutput[1]);
+		Command::slave->SerialSend(buffer);//servocommand;ID;POS;SPEED; //servoS?,1;100;50&5;0;100|10 //ID;POS;SPEED
+		std::cout << "The Arm is moving forward!!!:" << args[0] << "," << args[1] << std::endl;
 		return;
 	}
 	if (Command::type == "DriveStop") {
