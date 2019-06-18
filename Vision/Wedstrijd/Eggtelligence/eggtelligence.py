92245145ec2f612599315dd4046bfeb2fa58f056
Robin Hogerhuis <robin@mbp-van-robin.access.nhl.nl>
2019-06-18 12:25:39 +0200

    little change

--- Vision/Wedstrijd/Eggtelligence/eggtelligence.py
+++ Vision/Wedstrijd/Eggtelligence/eggtelligence.py
@@ -55,12 +55,12 @@ def qrDistance(frame, argument):
 
 def chicken(frame, argument):
 	imgTrainColor = cv2.imread('Images/TrainImg/Kip/piKip.jpg')
-	try:
-		from camera_pi import Camera_pi
-		screenWidth, screenHeight = Camera_pi.getSettings()
-	except:
-		from camera_opencv import Camera_opencv
-		screenWidth, screenHeight = Camera_opencv.getSettings()
+	# try:
+	# 	from camera_pi import Camera_pi
+	# 	screenWidth, screenHeight = Camera_pi.getSettings()
+	# except:
+	# 	from camera_opencv import Camera_opencv
+	# 	screenWidth, screenHeight = Camera_opencv.getSettings()
 
 	# imgTrainColor = cv2.resize(imgTrainColor, (screenWidth, screenHeight))
 	rectanglePts = fm_ORB(frame, imgTrainColor, 60)
