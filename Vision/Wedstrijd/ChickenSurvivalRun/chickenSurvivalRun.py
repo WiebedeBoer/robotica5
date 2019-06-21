d7639baefb83e32e6de632394e9b516db527359f
Robin Hogerhuis <robin@mbp-van-robin.access.nhl.nl>
2019-06-21 13:37:56 +0200

    update fm orb

--- Vision/Wedstrijd/ChickenSurvivalRun/chickenSurvivalRun.py
+++ Vision/Wedstrijd/ChickenSurvivalRun/chickenSurvivalRun.py
@@ -7,15 +7,6 @@ import cv2
 
 def chickenSurvivalRun(frame):
     imgTrainColor = cv2.imread('Images/TrainImg/Beker/piBeker.jpg')
-
-    try:
-        from camera_pi import Camera_pi
-        screenWidth, screenHeight = Camera_pi.getSettings()
-    except:
-        from camera_opencv import Camera_opencv
-        screenWidth, screenHeight = Camera_opencv.getSettings()
-
-    imgTrainColor = cv2.resize(imgTrainColor, (screenWidth, screenHeight))
     rectanglePts = fm_ORB(frame, imgTrainColor, 30)
 
     if rectanglePts is not False and rectanglePts is not None:
